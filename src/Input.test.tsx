e6e805e37606fb461c8893d5777f803a97243ba9
morenocantoj <morenocantoj@gmail.com>
2019-09-01 00:35:06 +0200

    Write tests for Input success=true

--- src/Input.test.tsx
+++ src/Input.test.tsx
@@ -19,10 +19,12 @@ const setup = ( initialState = {} ) => {
 describe('render', () => {
   describe('word has not been guessed', () => {
     let wrapper: any; // FIXME ShallowWrapper
+
     beforeEach(() => {
       const initialState = { success: false };
       wrapper = setup(initialState);
     });
+
     test('renders component without error', () => {
       const component = findByTestAttr(wrapper, 'component-input');
       expect(component.length).toBe(1);
@@ -37,14 +39,24 @@ describe('render', () => {
     });
   });
   describe('word has been guessed', () => {
-    test('renders component without error', () => {
+    let wrapper: any; // FIXME ShallowWrapper
 
+    beforeEach(() => {
+      const initialState = { success: true };
+      wrapper = setup(initialState);
+    });
+
+    test('renders component without error', () => {
+      const component = findByTestAttr(wrapper, 'component-input');
+      expect(component.length).toBe(1);
     });
     test('does not renders input box', () => {
-
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      expect(inputBox.length).toBe(0);
     });
     test('does not renders submit button', () => {
-
+      const submitButton = findByTestAttr(wrapper, 'submit-button');
+      expect(submitButton.length).toBe(0);
     });
   });
 });
