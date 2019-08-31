a48f056dbd2d7f7553757466eac522c20fc8d4fc
morenocantoj <morenocantoj@gmail.com>
2019-08-31 19:58:43 +0200

    Add Boostrap styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import GuessedWords from './GuessedWords';
+import Congrats from './Congrats';
+
 class App extends Component {
 
   render() {
     return (
-      <div data-test="component-app">
+      <div data-test="component-app" className="container">
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords guessedWords={[
+          { guessedWord: 'train', letterMatchCount: 3 }
+        ]} />
       </div>
     );
   }
--- src/Congrats.tsx
+++ src/Congrats.tsx
@@ -8,7 +8,7 @@ interface ICongratsProps {
 const Congrats = (props: ICongratsProps) => {
   if(props.success) {
     return (
-      <div data-test="component-congrats">
+      <div data-test="component-congrats" className="alert alert-success">
         <span data-test="congrats-message">
           Congratulations! You guessed the word!
         </span>
