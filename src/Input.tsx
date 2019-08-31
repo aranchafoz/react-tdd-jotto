f0425aefebd575a5805be8b3c4cb52e0ac581656
morenocantoj <morenocantoj@gmail.com>
2019-09-01 00:30:48 +0200

    Complete Input component render tests for success=false

--- src/Input.tsx
+++ src/Input.tsx
@@ -3,20 +3,40 @@ import { connect } from 'react-redux';
 
 interface IInputProps {
   store: any;
+  success: boolean;
 }
 
 class Input extends Component<IInputProps, {}> {
   render() {
+    const { success } = this.props;
+    const contents = success
+    ? null
+    : (
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          id="word-guess"
+          type="text"
+          placeholder="enter guess" />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          type="submit">
+          Submit
+        </button>
+      </form>
+    );
     return (
-      <div>
-        <button />
+      <div data-test="component-input">
+        { contents }
       </div>
     );
   }
 };
 
-const mapStateToProps = (state: any) => {
-  return {};
+const mapStateToProps = ({ success }: any) => {
+  return { success };
 };
 
 export default connect(mapStateToProps)(Input);
