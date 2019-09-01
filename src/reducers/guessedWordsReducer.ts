13e54ebe12c9ce0532aa91647ec36fbe31dec2e3
morenocantoj <morenocantoj@gmail.com>
2019-09-01 14:54:25 +0200

    Add Thunk for  piece of state

--- src/reducers/guessedWordsReducer.ts
+++ src/reducers/guessedWordsReducer.ts
@@ -1,9 +1,24 @@
+import { actionTypes } from '../actions';
+
+interface IAction {
+  type?: string;
+  payload?: {
+    guessedWord: string;
+    letterMatchCount: number;
+  };
+}
+
 /**
   * @function guessedWordsReducer
   * @param {array} state - Array of guessed words.
   * @param {object} action - action to be reduced.
   * @returns {boolean} - new guessedWords state.
 */
-export default (state: any = [], action: { type?: string }) => {
-  return null;
+export default (state: any = [], action: IAction) => {
+  switch(action.type) {
+    case actionTypes.GUESS_WORD:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
 };
---
+++ src/reducers/secretWordReducer.ts
@@ -0,0 +1,3 @@
+export default (state: any =null, action: any) => {
+  return state;
+}
