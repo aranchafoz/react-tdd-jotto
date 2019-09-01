95bb899b3c69fc8068fad7982a9645d044c4ae5c
morenocantoj <morenocantoj@gmail.com>
2019-09-01 13:07:53 +0200

    Set up guessWords tests

---
+++ src/reducers/guessedWordsReducer.ts
@@ -0,0 +1,9 @@
+/**
+  * @function guessedWordsReducer
+  * @param {array} state - Array of guessed words.
+  * @param {object} action - action to be reduced.
+  * @returns {boolean} - new guessedWords state.
+*/
+export default (state: any = [], action: { type?: string }) => {
+  return null;
+};
--- src/reducers/successReducer.ts
+++ src/reducers/successReducer.ts
@@ -2,11 +2,11 @@ import { actionTypes } from '../actions';
 
 /**
   * @function successReducer
-  * @param {array} state - Array of guessed words.
+  * @param {boolean} state - Previous success state.
   * @param {object} action - action to be reduced.
   * @returns {boolean} - new success state.
 */
-export default (state: any = false, action: { type?: string }) => {
+export default (state: boolean = false, action: { type?: string }) => {
   switch(action.type) {
     case (actionTypes.CORRECT_GUESS):
       return true;
